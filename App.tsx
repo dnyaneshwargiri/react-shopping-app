1da4682fec2277853cbd37b9ab328d12c634b2f3
dnyaneshwar giri <dnyaneshwargiri510@gmail.com>
2022-05-26 18:12:43 +0530

    added product containers

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
-import * as React from 'react';
-import './style.css';
+import * as React from "react";
+import "./style.css";
+import Header from "./src/containers/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from "./src/containers/ProductListing";
+import ProductDetails from "./src/containers/ProductDetails";
 
 export default function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetails />} />
+          <Route>404 Not Found</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,17 +1,18 @@
-import {ActionTypes} from "../constants/action-types.js";
-const initialState={
-  products:[{
-    id:1,
-    title:'Dnyaneshwar Giri',
-    category:'Software Engineer'
-  }]
-}
-export const productReducer=(state,{type,payload})=>{
-
-    switch(type){
-      case ActionTypes.SET_PRODUCTS:
-        return state;
-      default:
-        return state;
+import { ActionTypes } from "../constants/actions-types";
+const initialState = {
+  products: [
+    {
+      id: 1,
+      title: "Dnyaneshwar Giri",
+      category: "Software Engineer"
     }
-}
+  ]
+};
+export const productReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return state;
+    default:
+      return state;
+  }
+};
